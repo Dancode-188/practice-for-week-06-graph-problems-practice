5dd0a33b0f6259fae8374ffb214f930760407dc3
Dancode-188 <danbitengo@gmail.com>
2024-10-10 22:11:19 +0300

    Refactor getNeighbors and islandSize functions in island.js

--- island.js
+++ island.js
@@ -11,6 +11,12 @@ function getNeighbors(row, col, graph) {
   // Return neighbors
 
   // Your code here
+  let neighbors = [];
+  if (row > 0 && graph[row - 1][col] === 1) neighbors.push([row - 1, col]);
+  if (row < graph.length - 1 && graph[row + 1][col] === 1) neighbors.push([row + 1, col]);
+  if (col > 0 && graph[row][col - 1] === 1) neighbors.push([row, col - 1]);
+  if (col < graph[0].length - 1 && graph[row][col + 1] === 1) neighbors.push([row, col + 1]);
+  return neighbors;
 }
 
 
@@ -38,6 +44,24 @@ function islandSize(row, col, graph) {
   // return size
 
   // Your code here
+  let visited = new Set();
+  let stack = [[row, col]];
+  visited.add(`${row},${col}`);
+  let size = 0;
+
+  while (stack.length) {
+    let [currentRow, currentCol] = stack.pop();
+    size++;
+    let neighbors = getNeighbors(currentRow, currentCol, graph);
+    neighbors.forEach(([neighborRow, neighborCol]) => {
+      let neighborKey = `${neighborRow},${neighborCol}`;
+      if (!visited.has(neighborKey)) {
+        stack.push([neighborRow, neighborCol]);
+        visited.add(neighborKey);
+      }
+    });
+  }
+  return size;
 }
 
 module.exports = [getNeighbors, islandSize];
